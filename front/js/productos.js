cc988973c4ffaf79c9ccc7afd7ff3ad2faa50d81
sebasangelo <sebasangelo@hotmail.com>
2023-07-06 19:11:36 -0300

    subiendo todo el contenido

---
+++ front/js/productos.js
@@ -0,0 +1,43 @@
+const {createApp} = Vue 
+
+createApp({
+    data(){
+        return{
+            productos: [],
+            url: 'http://127.0.0.1:5000/productos',
+            cargando: true,
+            error: false
+        }
+    },
+
+    methods:{
+        fetchData(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.productos = data;
+                this.cargando = false;
+            })
+            .catch(err => {
+                console.error(err);
+                this.error = true;
+            })
+        },
+
+        eliminar(producto) {
+            const url = 'http://127.0.0.1:5000/productos/'+ producto;
+            let options = {
+                method: 'DELETE'
+            }
+            fetch(url, options)
+                .then(res => res.json())
+                .then(res => {
+                    location.reload();
+            })                     
+        }
+    },
+
+    created(){
+        this.fetchData(this.url);
+    }
+}).mount('#app')

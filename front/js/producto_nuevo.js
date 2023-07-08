d5d76d4658afccf9a00da8490f7d1bf2803d34bb
sebasangelo <sebasangelo@hotmail.com>
2023-07-08 14:56:55 -0300

    https

--- front/js/producto_nuevo.js
+++ front/js/producto_nuevo.js
@@ -15,7 +15,7 @@ function guardar(){
         imagen: i
     }
 
-    let url = 'http://127.0.0.1:5000/productos';
+    let url = 'https://127.0.0.1:5000/productos';
 
     let options ={
         body: JSON.stringify(producto),
--- front/js/productos.js
+++ front/js/productos.js
@@ -4,7 +4,7 @@ createApp({
     data(){
         return{
             productos: [],
-            url: 'http://127.0.0.1:5000/productos',
+            url: 'https://127.0.0.1:5000/productos',
             cargando: true,
             error: false
         }
@@ -25,7 +25,7 @@ createApp({
         },
 
         eliminar(producto) {
-            const url = 'http://127.0.0.1:5000/productos/'+ producto;
+            const url = 'https://127.0.0.1:5000/productos/'+ producto;
             let options = {
                 method: 'DELETE'
             }
